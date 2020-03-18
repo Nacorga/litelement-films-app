2c216f8a885cb7072f225bb9cf163c5576e0fd6a
CORTES GARCIA Ignacio <ignacio.cortes@soprasteria.com>
2020-03-18 09:43:45 +0100

    feat (search-page): API call test

--- films-app/test/searchFilm-page.test.js
+++ films-app/test/searchFilm-page.test.js
@@ -1,8 +1,17 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, aTimeout } from '@open-wc/testing';
+import Sinon, { stub } from 'sinon';
 
 import '../pages/search-films/search-films.js';
 
+let element;
+
 describe('Search page', () => {
+
+  beforeEach(async () => {
+    element = await fixture(html`
+      <search-films></search-films>
+    `);
+  });
   
   it('has default global props', async () => {
 
@@ -58,6 +67,37 @@ describe('Search page', () => {
     
     expect(el.loading).to.be.equal(true) &&
     expect(el.page).to.be.equal(4);
+
+  });
+
+  it(`"filmsApiCall" should return a list of films`, async () => {
+
+    const stubFetch = stub(window, 'fetch');
+    
+    const request = body => {
+
+      const mockResponse = new Response(JSON.stringify(body), {
+          status: 200,
+          headers: {
+              "Content-type": "application/json"
+          }
+      });
+
+      return Promise.resolve(mockResponse);
+
+    };
+
+    stubFetch.returns(Promise.resolve(request( { Search: ["test"], totalResults: 1 } )));
+
+    await element.filmsApiCall();
+
+    await aTimeout(1);
+
+    expect(element.films).to.eql(["test"]);
+    expect(element.totalResults).to.eql(1);
+    expect(element.loading).to.eql(false);
+
+    Sinon.restore();
 
   });
 
